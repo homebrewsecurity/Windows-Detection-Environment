4d272819675a232e0bd05a8d48b39da7c8f8bea6
Logan Bennett <112832242+homebrewsecurity@users.noreply.github.com>
2026-02-26 21:21:46 -0500

    Refactor Active Directory installation script

--- domain_controller/60-Install-ActiveDirectory.ps1
+++ domain_controller/60-Install-ActiveDirectory.ps1
@@ -4,20 +4,13 @@ $DomainData = $Data.Domain
 $ScriptName = $MyInvocation.MyCommand.Name
 
 # Code
-$IsInstalled = (Get-WindowsFeature 'AD-Domain-Services' | Where-Object {$_.InstallState -eq 'Installed'})
 
-if ($IsInstalled)
-{
-    Write-Host "$ScriptName`: Active Directory services already installed." -ForegroundColor Yellow
-}
-else
-{
-    # Installs the feature and imports ADDSDeployment
-    Install-WindowsFeature 'AD-Domain-Services' -IncludeManagementTools
-    Import-Module ADDSDeployment
+# Installs the feature and imports ADDSDeployment
+Install-WindowsFeature 'AD-Domain-Services' -IncludeManagementTools
+Import-Module ADDSDeployment
 
-    # Promotes to DC and created forest
-    Install-ADDSForest -DomainName $DomainData.DomainName -InstallDNS:$true
+# Promotes to DC and created forest
+Install-ADDSForest -DomainName $DomainData.DomainName -InstallDNS:$true
 
-    $RequiresRestart = $True
-}
+$RequiresRestart = $True
+
